b383c5a725bd80d655e66b1f1cfa0c7fc80f31da
GraceOnwere <graceonwere.ng@gmail.com>
2026-04-20 10:54:55 +0100

    Added multiplicationtable file

--- Classwork/multiplicationtable.py
+++ Classwork/multiplicationtable.py
@@ -3,15 +3,14 @@ initialize variables
 collect input from user
 use a loop to show the muliplication table
 '''
-count = 0
+
 number = int(input('Enter a number: '))
 
 for value in range (1,11):
-    count = count + 1
    
-    muliplication = number * count
+    multiplication = number * value
 
-    print(f"{number} x {count} = {muliplication}")
+    print(f"{number} x {value} = {multiplication}")
      
        
 
